a055ff402e566da70543f4f083f32bb1c75c036a
kartik1023 <kartikpotdar007@gmail.com>
2024-02-08 13:50:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Groceries</title>
+   <!-- style and other link --> 
+    <?php include"./includes/link.php";?>
+
+</head>
+<body>
+<!-- navbar -->
+<?php include "./includes/navbar.php";?>
+
+
+
+
+
+
+
+
+<!-- footer -->
+<?php include"./includes/footer.php";?>
+ <!-- script -->
+<?php include"./includes/scripts.php";?>
+</body>
+</html>
